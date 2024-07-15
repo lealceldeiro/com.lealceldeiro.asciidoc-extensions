6b9ad0534ea6bba1d84fe43f6d21642a9c153912
Asiel Leal Celdeiro <15990580+lealceldeiro@users.noreply.github.com>
2024-07-15 20:11:20 +0200

    Initial commit

---
+++ src/main/java/com/lealceldeiro/asciidoc/extensions/CalcMacro.java
@@ -0,0 +1,49 @@
+package com.lealceldeiro.asciidoc.extensions;
+
+import org.asciidoctor.ast.ContentNode;
+import org.asciidoctor.extension.InlineMacroProcessor;
+import org.asciidoctor.extension.Name;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+/**
+ * Docs at
+ * <a href="https://docs.asciidoctor.org/asciidoctorj/latest/extensions/extensions-introduction/">
+ * AsciidocJ Extensions API
+ * </a>
+ */
+@Name("calc")
+public class CalcMacro extends InlineMacroProcessor {
+  private static final Logger LOGGER = Logger.getLogger(CalcMacro.class.getName());
+
+  @Override
+  public Object process(ContentNode parent, String target, Map<String, Object> attributes) {
+    String href =
+        new StringBuilder()
+            .append("https://github.com/")
+            .append(attributes.get("repo"))
+            .append("/issues/")
+            .append(target).toString();
+
+    Map<String, Object> options = new HashMap<>();
+    options.put("type", ":link");
+    options.put("target", href);
+    return createPhraseNode(parent, "anchor", target, attributes, options);
+  }
+
+//  @Override
+  public Object aprocess(ContentNode parent, String target, Map<String, Object> attributes) {
+    String calcResult = calculate(target);
+
+    Map<String, Object> options = new HashMap<>();
+
+    return createPhraseNode(parent,"text", calcResult, options);
+  }
+
+  private String calculate(String operation) {
+    LOGGER.info("OPERATION: " + operation);
+    return "operation is " + operation;
+  }
+}
